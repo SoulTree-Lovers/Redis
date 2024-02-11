1662f0fd98e2f60f39747cd02bd79b29f5ab64be
kangseungmin <rkdtmdals999@naver.com>
2024-02-11 22:18:14 +0900

    [feat] Redis cache에 데이터 저장하기

--- src/main/kotlin/com/example/redis/service/NoticeService.kt
+++ src/main/kotlin/com/example/redis/service/NoticeService.kt
@@ -1,6 +1,7 @@
 package com.example.redis.service
 
 import com.example.redis.common.Log
+import com.example.redis.model.NoticeDto
 import com.example.redis.repository.NoticeRepository
 import org.springframework.cache.annotation.CachePut
 import org.springframework.cache.annotation.Cacheable
@@ -18,15 +19,15 @@ class NoticeService (
      * notice::[인자값]이 key가 된다.
      * value는 return하는 값으로 지정된다.
      */
-    @Cacheable(cacheNames = ["notice"], key = "#notice")
-    fun getNotice(notice: String?): String? {
-        log.info("notice service get notice: {}", notice)
-        return noticeRepository.getNotice(notice)
+    @Cacheable(cacheNames = ["notice"], key = "#id")
+    fun getNotice(id: Long?): NoticeDto? {
+        log.info("notice service get notice: {}", id)
+        return noticeRepository.getNotice(id)
     }
 
     // redis 캐시에 저장
-    @CachePut(cacheNames = ["notice"], key = "#notice") // #notice는 파라미터 이름과 매핑
-    fun addNotice(notice: String?): String? {
+    @CachePut(cacheNames = ["notice"], key = "#notice.id") // #notice는 파라미터 이름과 매핑
+    fun addNotice(notice: NoticeDto?): NoticeDto? {
         log.info("notice service add notice: {}", notice)
         return noticeRepository.addNotice(notice)
     }
