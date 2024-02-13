5442370ace5b9c3db6af4764c2521ddd1bf2777b
kangseungmin <rkdtmdals999@naver.com>
2024-02-13 15:46:05 +0900

    [feat] Notice repository 구현

--- src/main/kotlin/com/example/redis/repository/NoticeRepository.kt
+++ src/main/kotlin/com/example/redis/repository/NoticeRepository.kt
@@ -1,6 +1,8 @@
 package com.example.redis.repository
 
 import com.example.redis.common.Log
+import com.example.redis.model.NoticeDto
+import com.example.redis.service.NoticeService
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,17 +11,25 @@ class NoticeRepository {
     // Logger 불러오기
     companion object: Log
 
-    private val noticeList = mutableListOf<String>()
+    private val noticeList = mutableListOf<NoticeDto>()
 
     // redis 캐시 사용
-    fun getNotice(notice: String?): String? {
-        log.info("get notice method call: {}", notice)
-        return noticeList.filter { it -> it == notice }.firstOrNull()
+    fun getNotice(id: Long?): NoticeDto? {
+        log.info("get notice method call: {}", id)
+        return noticeList.filter { it -> it.id == id }.firstOrNull()
     }
 
-    fun addNotice(notice: String?): String? {
+    fun addNotice(notice: NoticeDto?): NoticeDto? {
         log.info("repository add notice method call: {}", notice)
-        notice?.let { noticeList.add(it) }
-        return notice
+
+        val index = noticeList.size + 1
+
+        return notice?.apply {
+            this.id = index.toLong()
+        }?.apply { // 받은 데이터를 그대로 리턴
+            noticeList.add(this)
+        }.also { // 받은 데이터를 그대로 리턴
+            log.info("save Dto: {}", it)
+        }
     }
 }
